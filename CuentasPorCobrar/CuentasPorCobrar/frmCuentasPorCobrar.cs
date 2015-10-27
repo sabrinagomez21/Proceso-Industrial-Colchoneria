06a174cacf09158a6dfe298690b60ee6c35d05d3
Douglas-KCA <kevin-dou@outlook.com>
2015-10-26 21:43:21 -0600

    Cuentas por Cobrar y Creacion Bancos

---
+++ CuentasPorCobrar/CuentasPorCobrar/frmCuentasPorCobrar.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CuentasPorCobrar
+{
+    public partial class frmCuentasPorCobrar : Form
+    {
+        public frmCuentasPorCobrar()
+        {
+            InitializeComponent();
+            grdCuentasporcobrar.Rows.Add("001", "Juan", "022", "26/10/2015", "1000", "Cancelado");
+            grdCuentasporcobrar.Rows.Add("002", "Pedro", "023", "26/10/2015", "20000", "Activo");
+        }
+
+        private void grdCuentasporcobrar_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            frmTipoCobro ver = new frmTipoCobro();
+            ver.Show();
+        }
+    }
+}
