18e16ca83c1e7d3b27d710b74589e130211e3366
Dennys Iván <navi93@live.com>
2015-10-26 18:02:20 -0600

    Prototipo no funcional
    Formulario de verificación de documentos por pagar y los medios de pagos

---
+++ Cuentas por pagar/cuentasporpagar/cuentasporpagar/frmCuentasxpagar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cuentasporpagar
+{
+    public partial class frmCuentasxpagar : Form
+    {
+        public frmCuentasxpagar()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProveedor_Click(object sender, EventArgs e)
+        {
+            frmProveedor frm = new frmProveedor();
+            frm.Show();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmPagos frm = new frmPagos();
+            frm.Show();
+        }
+    }
+}
