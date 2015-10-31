59e5ad5ae5768a9d92ed3056ee137bc20a2f2d89
Jrevmen <j.revmen@gmail.com>
2015-10-31 09:55:42 -0600

    Sin Cambios

---
+++ CuentasPorCobrar/CuentasPorCobrar/D_CxC.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CuentasPorCobrar
+{
+    class D_CxC: CAD
+    {
+        public static int Insertar(E_CxC cxc){
+            return 0;
+        }
+
+        public static int Actualizar(E_CxC cxc){
+            return 0;
+        }
+
+        public static int Eliminar(E_CxC cxc){
+            return 0;
+        }
+
+        public E_CxC Consulta(E_CxC cxc){
+            return cxc;
+        }
+
+        public static DataTable ObtenerRegistros()
+        {
+            DataTable dtRegistros = new DataTable();
+            return dtRegistros;
+        }
+    }
+}
