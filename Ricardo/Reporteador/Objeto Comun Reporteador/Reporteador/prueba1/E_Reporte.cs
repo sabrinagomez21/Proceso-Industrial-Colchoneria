e4ac309a2e907c6db0893be86c0cb1a1ed26b1e8
vonhacker <vonraynts@gmail.com>
2015-11-02 17:51:31 -0600

    nuevos cambios

---
+++ Ricardo/Reporteador/Objeto Comun Reporteador/Reporteador/prueba1/E_Reporte.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prueba1
+{
+    #region Class E_Reporte
+    public class E_Reporte
+    {
+    #endregion
+        #region Atributos y Encapsulamiento
+        public string id { get; set; }
+        public string nom_reporte { get; set; }
+        public string usuario { get; set; }
+        public string fecha_hora { get; set; }
+
+        #endregion
+
+        #region E_Reporte
+        public E_Reporte()
+        {
+
+        }
+        #endregion
+
+    }}
