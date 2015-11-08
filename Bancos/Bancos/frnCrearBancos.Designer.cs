252626bb5393c91b760525c7051468645e0d98b9
Douglas-KCA <kevin-dou@outlook.com>
2015-11-08 15:04:19 -0600

    Diseño Crear Bancos

---
+++ Bancos/Bancos/frnCrearBancos.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace Bancos
+{
+    partial class frnCrearBancos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frnCrearBancos));
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.btnIrUltimo = new System.Windows.Forms.Button();
+            this.btnSiguiente = new System.Windows.Forms.Button();
+            this.btnAnterior = new System.Windows.Forms.Button();
+            this.btnIrPrimero = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.btnRefrescar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.pnlInformacion = new System.Windows.Forms.Panel();
+            this.lblConciliacion = new System.Windows.Forms.Label();
+            this.lblEncargado = new System.Windows.Forms.Label();
+            this.txtEncargado = new System.Windows.Forms.TextBox();
+            this.txtConciliacion = new System.Windows.Forms.TextBox();
+            this.txtCuenta = new System.Windows.Forms.TextBox();
+            this.lblCuenta = new System.Windows.Forms.Label();
+            this.Verificacion = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Direccion = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Estado = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.pnlInformacion.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Controls.Add(this.btnIrUltimo);
+            this.panel3.Controls.Add(this.btnSiguiente);
+            this.panel3.Controls.Add(this.btnAnterior);
+            this.panel3.Controls.Add(this.btnIrPrimero);
+            this.panel3.Controls.Add(this.btnBuscar);
+            this.panel3.Controls.Add(this.btnImprimir);
+            this.panel3.Controls.Add(this.btnRefrescar);
+            this.panel3.Controls.Add(this.btnCancelar);
+            this.panel3.Controls.Add(this.btnGuardar);
+            this.panel3.Controls.Add(this.btnEliminar);
+            this.panel3.Controls.Add(this.btnEditar);
+            this.panel3.Controls.Add(this.btnNuevo);
+            this.panel3.Location = new System.Drawing.Point(135, 12);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(653, 51);
+            this.panel3.TabIndex = 16;
+            // 
+            // btnIrUltimo
+            // 
+            this.btnIrUltimo.Image = ((System.Drawing.Image)(resources.GetObject("btnIrUltimo.Image")));
+            this.btnIrUltimo.Location = new System.Drawing.Point(597, 4);
+            this.btnIrUltimo.Name = "btnIrUltimo";
+            this.btnIrUltimo.Size = new System.Drawing.Size(48, 42);
+            this.btnIrUltimo.TabIndex = 16;
+            this.btnIrUltimo.UseVisualStyleBackColor = true;
+            // 
+            // btnSiguiente
+            // 
+            this.btnSiguiente.Image = ((System.Drawing.Image)(resources.GetObject("btnSiguiente.Image")));
+            this.btnSiguiente.Location = new System.Drawing.Point(543, 4);
+            this.btnSiguiente.Name = "btnSiguiente";
+            this.btnSiguiente.Size = new System.Drawing.Size(48, 42);
+            this.btnSiguiente.TabIndex = 15;
+            this.btnSiguiente.UseVisualStyleBackColor = true;
+            // 
+            // btnAnterior
+            // 
+            this.btnAnterior.Image = ((System.Drawing.Image)(resources.GetObject("btnAnterior.Image")));
+            this.btnAnterior.Location = new System.Drawing.Point(489, 4);
+            this.btnAnterior.Name = "btnAnterior";
+            this.btnAnterior.Size = new System.Drawing.Size(48, 42);
+            this.btnAnterior.TabIndex = 14;
+            this.btnAnterior.UseVisualStyleBackColor = true;
+            // 
+            // btnIrPrimero
+            // 
+            this.btnIrPrimero.Image = ((System.Drawing.Image)(resources.GetObject("btnIrPrimero.Image")));
+            this.btnIrPrimero.Location = new System.Drawing.Point(435, 3);
+            this.btnIrPrimero.Name = "btnIrPrimero";
+            this.btnIrPrimero.Size = new System.Drawing.Size(48, 42);
+            this.btnIrPrimero.TabIndex = 13;
+            this.btnIrPrimero.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Image = ((System.Drawing.Image)(resources.GetObject("btnBuscar.Image")));
+            this.btnBuscar.Location = new System.Drawing.Point(381, 4);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(48, 42);
+            this.btnBuscar.TabIndex = 12;
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.Image = ((System.Drawing.Image)(resources.GetObject("btnImprimir.Image")));
+            this.btnImprimir.Location = new System.Drawing.Point(327, 4);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(48, 42);
+            this.btnImprimir.TabIndex = 11;
+            this.btnImprimir.UseVisualStyleBackColor = true;
+            // 
+            // btnRefrescar
+            // 
+            this.btnRefrescar.Image = ((System.Drawing.Image)(resources.GetObject("btnRefrescar.Image")));
+            this.btnRefrescar.Location = new System.Drawing.Point(273, 4);
+            this.btnRefrescar.Name = "btnRefrescar";
+            this.btnRefrescar.Size = new System.Drawing.Size(48, 42);
+            this.btnRefrescar.TabIndex = 10;
+            this.btnRefrescar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Image = ((System.Drawing.Image)(resources.GetObject("btnCancelar.Image")));
+            this.btnCancelar.Location = new System.Drawing.Point(219, 4);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(48, 42);
+            this.btnCancelar.TabIndex = 9;
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Image = ((System.Drawing.Image)(resources.GetObject("btnGuardar.Image")));
+            this.btnGuardar.Location = new System.Drawing.Point(165, 4);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(48, 42);
+            this.btnGuardar.TabIndex = 8;
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Image = ((System.Drawing.Image)(resources.GetObject("btnEliminar.Image")));
+            this.btnEliminar.Location = new System.Drawing.Point(111, 4);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(48, 42);
+            this.btnEliminar.TabIndex = 7;
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.Image = ((System.Drawing.Image)(resources.GetObject("btnEditar.Image")));
+            this.btnEditar.Location = new System.Drawing.Point(57, 3);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(48, 42);
+            this.btnEditar.TabIndex = 6;
+            this.btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Image = ((System.Drawing.Image)(resources.GetObject("btnNuevo.Image")));
+            this.btnNuevo.Location = new System.Drawing.Point(3, 4);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(48, 42);
+            this.btnNuevo.TabIndex = 5;
+            this.btnNuevo.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Verificacion,
+            this.Nombre,
+            this.Direccion,
+            this.Estado});
+            this.dataGridView1.Location = new System.Drawing.Point(40, 198);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(847, 261);
+            this.dataGridView1.TabIndex = 18;
+            // 
+            // pnlInformacion
+            // 
+            this.pnlInformacion.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlInformacion.Controls.Add(this.lblConciliacion);
+            this.pnlInformacion.Controls.Add(this.lblEncargado);
+            this.pnlInformacion.Controls.Add(this.txtEncargado);
+            this.pnlInformacion.Controls.Add(this.txtConciliacion);
+            this.pnlInformacion.Controls.Add(this.txtCuenta);
+            this.pnlInformacion.Controls.Add(this.lblCuenta);
+            this.pnlInformacion.Location = new System.Drawing.Point(40, 83);
+            this.pnlInformacion.Name = "pnlInformacion";
+            this.pnlInformacion.Size = new System.Drawing.Size(847, 100);
+            this.pnlInformacion.TabIndex = 17;
+            // 
+            // lblConciliacion
+            // 
+            this.lblConciliacion.AutoSize = true;
+            this.lblConciliacion.Location = new System.Drawing.Point(429, 27);
+            this.lblConciliacion.Name = "lblConciliacion";
+            this.lblConciliacion.Size = new System.Drawing.Size(55, 13);
+            this.lblConciliacion.TabIndex = 14;
+            this.lblConciliacion.Text = "Direccion:";
+            // 
+            // lblEncargado
+            // 
+            this.lblEncargado.AutoSize = true;
+            this.lblEncargado.Location = new System.Drawing.Point(63, 27);
+            this.lblEncargado.Name = "lblEncargado";
+            this.lblEncargado.Size = new System.Drawing.Size(47, 13);
+            this.lblEncargado.TabIndex = 11;
+            this.lblEncargado.Text = "Nombre:";
+            // 
+            // txtEncargado
+            // 
+            this.txtEncargado.Location = new System.Drawing.Point(149, 24);
+            this.txtEncargado.Name = "txtEncargado";
+            this.txtEncargado.Size = new System.Drawing.Size(257, 20);
+            this.txtEncargado.TabIndex = 12;
+            // 
+            // txtConciliacion
+            // 
+            this.txtConciliacion.Location = new System.Drawing.Point(520, 24);
+            this.txtConciliacion.Name = "txtConciliacion";
+            this.txtConciliacion.Size = new System.Drawing.Size(126, 20);
+            this.txtConciliacion.TabIndex = 15;
+            // 
+            // txtCuenta
+            // 
+            this.txtCuenta.Location = new System.Drawing.Point(149, 55);
+            this.txtCuenta.Name = "txtCuenta";
+            this.txtCuenta.Size = new System.Drawing.Size(163, 20);
+            this.txtCuenta.TabIndex = 17;
+            this.txtCuenta.Text = "Activo";
+            // 
+            // lblCuenta
+            // 
+            this.lblCuenta.AutoSize = true;
+            this.lblCuenta.Location = new System.Drawing.Point(66, 58);
+            this.lblCuenta.Name = "lblCuenta";
+            this.lblCuenta.Size = new System.Drawing.Size(43, 13);
+            this.lblCuenta.TabIndex = 16;
+            this.lblCuenta.Text = "Estado:";
+            // 
+            // Verificacion
+            // 
+            this.Verificacion.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.Verificacion.HeaderText = "Verificacion";
+            this.Verificacion.Name = "Verificacion";
+            this.Verificacion.Width = 68;
+            // 
+            // Nombre
+            // 
+            this.Nombre.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            this.Nombre.ReadOnly = true;
+            // 
+            // Direccion
+            // 
+            this.Direccion.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Direccion.HeaderText = "Direccion";
+            this.Direccion.Name = "Direccion";
+            this.Direccion.ReadOnly = true;
+            // 
+            // Estado
+            // 
+            this.Estado.HeaderText = "Estado";
+            this.Estado.Items.AddRange(new object[] {
+            "Quetzales ",
+            "Dolares",
+            "Euros",
+            "Libras",
+            "Yenes"});
+            this.Estado.Name = "Estado";
+            // 
+            // frnCrearBancos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(925, 484);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.pnlInformacion);
+            this.Controls.Add(this.panel3);
+            this.Name = "frnCrearBancos";
+            this.Text = "Crear Bancos";
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.pnlInformacion.ResumeLayout(false);
+            this.pnlInformacion.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button btnIrUltimo;
+        private System.Windows.Forms.Button btnSiguiente;
+        private System.Windows.Forms.Button btnAnterior;
+        private System.Windows.Forms.Button btnIrPrimero;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnImprimir;
+        private System.Windows.Forms.Button btnRefrescar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel pnlInformacion;
+        private System.Windows.Forms.Label lblConciliacion;
+        private System.Windows.Forms.Label lblEncargado;
+        private System.Windows.Forms.TextBox txtEncargado;
+        private System.Windows.Forms.TextBox txtConciliacion;
+        private System.Windows.Forms.TextBox txtCuenta;
+        private System.Windows.Forms.Label lblCuenta;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Verificacion;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Direccion;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Estado;
+    }
+}
