56e48e6fb6b715057cfa948105129ffe63532d7b
Douglas-KCA <kevin-dou@outlook.com>
2015-11-08 22:20:25 -0600

    Programacion Navegador en crear Bancos

--- Bancos/Bancos/frnCrearBancos.Designer.cs
+++ Bancos/Bancos/frnCrearBancos.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Bancos
 {
-    partial class frnCrearBancos
+    partial class frmCrearBancos
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frnCrearBancos));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCrearBancos));
             this.panel3 = new System.Windows.Forms.Panel();
             this.btnIrUltimo = new System.Windows.Forms.Button();
             this.btnSiguiente = new System.Windows.Forms.Button();
@@ -43,17 +43,18 @@
             this.btnEditar = new System.Windows.Forms.Button();
             this.btnNuevo = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.pnlInformacion = new System.Windows.Forms.Panel();
-            this.lblConciliacion = new System.Windows.Forms.Label();
-            this.lblEncargado = new System.Windows.Forms.Label();
-            this.txtEncargado = new System.Windows.Forms.TextBox();
-            this.txtConciliacion = new System.Windows.Forms.TextBox();
-            this.txtCuenta = new System.Windows.Forms.TextBox();
-            this.lblCuenta = new System.Windows.Forms.Label();
             this.Verificacion = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Direccion = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Estado = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.pnlInformacion = new System.Windows.Forms.Panel();
+            this.lblConciliacion = new System.Windows.Forms.Label();
+            this.lblEncargado = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtEstado = new System.Windows.Forms.TextBox();
+            this.lblCuenta = new System.Windows.Forms.Label();
+            this.cmbEstado = new System.Windows.Forms.ComboBox();
             this.panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.pnlInformacion.SuspendLayout();
@@ -150,6 +151,7 @@
             this.btnCancelar.Size = new System.Drawing.Size(48, 42);
             this.btnCancelar.TabIndex = 9;
             this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
             // 
             // btnGuardar
             // 
@@ -159,6 +161,7 @@
             this.btnGuardar.Size = new System.Drawing.Size(48, 42);
             this.btnGuardar.TabIndex = 8;
             this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
             // 
             // btnEliminar
             // 
@@ -186,6 +189,7 @@
             this.btnNuevo.Size = new System.Drawing.Size(48, 42);
             this.btnNuevo.TabIndex = 5;
             this.btnNuevo.UseVisualStyleBackColor = true;
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
             // 
             // dataGridView1
             // 
@@ -199,69 +203,6 @@
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(847, 261);
             this.dataGridView1.TabIndex = 18;
-            // 
-            // pnlInformacion
-            // 
-            this.pnlInformacion.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pnlInformacion.Controls.Add(this.lblConciliacion);
-            this.pnlInformacion.Controls.Add(this.lblEncargado);
-            this.pnlInformacion.Controls.Add(this.txtEncargado);
-            this.pnlInformacion.Controls.Add(this.txtConciliacion);
-            this.pnlInformacion.Controls.Add(this.txtCuenta);
-            this.pnlInformacion.Controls.Add(this.lblCuenta);
-            this.pnlInformacion.Location = new System.Drawing.Point(40, 83);
-            this.pnlInformacion.Name = "pnlInformacion";
-            this.pnlInformacion.Size = new System.Drawing.Size(847, 100);
-            this.pnlInformacion.TabIndex = 17;
-            // 
-            // lblConciliacion
-            // 
-            this.lblConciliacion.AutoSize = true;
-            this.lblConciliacion.Location = new System.Drawing.Point(429, 27);
-            this.lblConciliacion.Name = "lblConciliacion";
-            this.lblConciliacion.Size = new System.Drawing.Size(55, 13);
-            this.lblConciliacion.TabIndex = 14;
-            this.lblConciliacion.Text = "Direccion:";
-            // 
-            // lblEncargado
-            // 
-            this.lblEncargado.AutoSize = true;
-            this.lblEncargado.Location = new System.Drawing.Point(63, 27);
-            this.lblEncargado.Name = "lblEncargado";
-            this.lblEncargado.Size = new System.Drawing.Size(47, 13);
-            this.lblEncargado.TabIndex = 11;
-            this.lblEncargado.Text = "Nombre:";
-            // 
-            // txtEncargado
-            // 
-            this.txtEncargado.Location = new System.Drawing.Point(149, 24);
-            this.txtEncargado.Name = "txtEncargado";
-            this.txtEncargado.Size = new System.Drawing.Size(257, 20);
-            this.txtEncargado.TabIndex = 12;
-            // 
-            // txtConciliacion
-            // 
-            this.txtConciliacion.Location = new System.Drawing.Point(520, 24);
-            this.txtConciliacion.Name = "txtConciliacion";
-            this.txtConciliacion.Size = new System.Drawing.Size(126, 20);
-            this.txtConciliacion.TabIndex = 15;
-            // 
-            // txtCuenta
-            // 
-            this.txtCuenta.Location = new System.Drawing.Point(149, 55);
-            this.txtCuenta.Name = "txtCuenta";
-            this.txtCuenta.Size = new System.Drawing.Size(163, 20);
-            this.txtCuenta.TabIndex = 17;
-            this.txtCuenta.Text = "Activo";
-            // 
-            // lblCuenta
-            // 
-            this.lblCuenta.AutoSize = true;
-            this.lblCuenta.Location = new System.Drawing.Point(66, 58);
-            this.lblCuenta.Name = "lblCuenta";
-            this.lblCuenta.Size = new System.Drawing.Size(43, 13);
-            this.lblCuenta.TabIndex = 16;
-            this.lblCuenta.Text = "Estado:";
             // 
             // Verificacion
             // 
@@ -295,7 +236,89 @@
             "Yenes"});
             this.Estado.Name = "Estado";
             // 
-            // frnCrearBancos
+            // pnlInformacion
+            // 
+            this.pnlInformacion.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlInformacion.Controls.Add(this.cmbEstado);
+            this.pnlInformacion.Controls.Add(this.lblConciliacion);
+            this.pnlInformacion.Controls.Add(this.lblEncargado);
+            this.pnlInformacion.Controls.Add(this.txtNombre);
+            this.pnlInformacion.Controls.Add(this.txtDireccion);
+            this.pnlInformacion.Controls.Add(this.txtEstado);
+            this.pnlInformacion.Controls.Add(this.lblCuenta);
+            this.pnlInformacion.Location = new System.Drawing.Point(40, 83);
+            this.pnlInformacion.Name = "pnlInformacion";
+            this.pnlInformacion.Size = new System.Drawing.Size(847, 100);
+            this.pnlInformacion.TabIndex = 17;
+            // 
+            // lblConciliacion
+            // 
+            this.lblConciliacion.AutoSize = true;
+            this.lblConciliacion.Location = new System.Drawing.Point(429, 27);
+            this.lblConciliacion.Name = "lblConciliacion";
+            this.lblConciliacion.Size = new System.Drawing.Size(55, 13);
+            this.lblConciliacion.TabIndex = 14;
+            this.lblConciliacion.Text = "Direccion:";
+            // 
+            // lblEncargado
+            // 
+            this.lblEncargado.AutoSize = true;
+            this.lblEncargado.Location = new System.Drawing.Point(63, 27);
+            this.lblEncargado.Name = "lblEncargado";
+            this.lblEncargado.Size = new System.Drawing.Size(47, 13);
+            this.lblEncargado.TabIndex = 11;
+            this.lblEncargado.Text = "Nombre:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Enabled = false;
+            this.txtNombre.Location = new System.Drawing.Point(149, 24);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(257, 20);
+            this.txtNombre.TabIndex = 12;
+            this.txtNombre.Tag = "vnombrebanco";
+            // 
+            // txtDireccion
+            // 
+            this.txtDireccion.Enabled = false;
+            this.txtDireccion.Location = new System.Drawing.Point(520, 24);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(126, 20);
+            this.txtDireccion.TabIndex = 15;
+            this.txtDireccion.Tag = "vdireccion";
+            // 
+            // txtEstado
+            // 
+            this.txtEstado.Enabled = false;
+            this.txtEstado.Location = new System.Drawing.Point(342, 56);
+            this.txtEstado.Name = "txtEstado";
+            this.txtEstado.Size = new System.Drawing.Size(51, 20);
+            this.txtEstado.TabIndex = 17;
+            this.txtEstado.Tag = "cestado";
+            // 
+            // lblCuenta
+            // 
+            this.lblCuenta.AutoSize = true;
+            this.lblCuenta.Location = new System.Drawing.Point(66, 58);
+            this.lblCuenta.Name = "lblCuenta";
+            this.lblCuenta.Size = new System.Drawing.Size(43, 13);
+            this.lblCuenta.TabIndex = 16;
+            this.lblCuenta.Text = "Estado:";
+            // 
+            // cmbEstado
+            // 
+            this.cmbEstado.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbEstado.Enabled = false;
+            this.cmbEstado.FormattingEnabled = true;
+            this.cmbEstado.Items.AddRange(new object[] {
+            "Activo",
+            "Inactivo"});
+            this.cmbEstado.Location = new System.Drawing.Point(149, 55);
+            this.cmbEstado.Name = "cmbEstado";
+            this.cmbEstado.Size = new System.Drawing.Size(187, 21);
+            this.cmbEstado.TabIndex = 20;
+            // 
+            // frmCrearBancos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
@@ -303,7 +326,7 @@
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.pnlInformacion);
             this.Controls.Add(this.panel3);
-            this.Name = "frnCrearBancos";
+            this.Name = "frmCrearBancos";
             this.Text = "Crear Bancos";
             this.panel3.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -332,13 +355,14 @@
         private System.Windows.Forms.Panel pnlInformacion;
         private System.Windows.Forms.Label lblConciliacion;
         private System.Windows.Forms.Label lblEncargado;
-        private System.Windows.Forms.TextBox txtEncargado;
-        private System.Windows.Forms.TextBox txtConciliacion;
-        private System.Windows.Forms.TextBox txtCuenta;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtEstado;
         private System.Windows.Forms.Label lblCuenta;
         private System.Windows.Forms.DataGridViewCheckBoxColumn Verificacion;
         private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
         private System.Windows.Forms.DataGridViewTextBoxColumn Direccion;
         private System.Windows.Forms.DataGridViewComboBoxColumn Estado;
+        private System.Windows.Forms.ComboBox cmbEstado;
     }
 }
--- Bancos/Bancos/frnCrearBancos.cs
+++ Bancos/Bancos/frnCrearBancos.cs
@@ -7,14 +7,54 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Navegador;
 
 namespace Bancos
 {
-    public partial class frnCrearBancos : Form
+    public partial class frmCrearBancos : Form
     {
-        public frnCrearBancos()
+        public frmCrearBancos()
         {
             InitializeComponent();
+            cmbEstado.SelectedIndex = 0;
+            System.Console.WriteLine("esto es lo que tiene---- "+cmbEstado.SelectedItem.ToString());
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            clasnegocio cnegocio = new clasnegocio();
+            cnegocio.funactivarDesactivarTextbox(txtNombre, true); 
+            cnegocio.funactivarDesactivarTextbox(txtDireccion, true);
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            string tabla = "mabanco";
+            Boolean permiso = true;
+            txtEstado.Text = cmbEstado.SelectedItem.ToString();
+            TextBox[] datos = { txtNombre, txtDireccion, txtEstado };
+
+            clasnegocio cn = new clasnegocio();
+            cn.AsignarObjetos(tabla, permiso, datos);
+            funLimpiar();
+        }
+
+        void funLimpiar(){
+            txtDireccion.Text = "";
+            txtNombre.Text = "";
+            txtEstado.Text = "";
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            clasnegocio cnegocio = new clasnegocio();
+            cnegocio.funactivarDesactivarTextbox(txtNombre, false);
+            cnegocio.funactivarDesactivarTextbox(txtDireccion, false);
+        }
+
+        void funLlenarTabla()
+        {
+
         }
     }
 }
