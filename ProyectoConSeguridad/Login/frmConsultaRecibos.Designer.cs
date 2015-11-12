70010f21f92aad3b6d6fd0ba132e735748eda22e
Dylan Corado <nos_196@hotmail.com>
2015-11-12 02:00:39 -0700

    sin cambios

---
+++ ProyectoConSeguridad/Login/frmConsultaRecibos.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace Login
+{
+    partial class frmConsultaRecibos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmConsultaRecibos));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnirUltimo = new System.Windows.Forms.Button();
+            this.btnsiguiente = new System.Windows.Forms.Button();
+            this.btnanterior = new System.Windows.Forms.Button();
+            this.btnirPrimero = new System.Windows.Forms.Button();
+            this.btnbuscar = new System.Windows.Forms.Button();
+            this.btnimprimir = new System.Windows.Forms.Button();
+            this.btnrefrescar = new System.Windows.Forms.Button();
+            this.btncancelar = new System.Windows.Forms.Button();
+            this.btnguardar = new System.Windows.Forms.Button();
+            this.btneliminar = new System.Windows.Forms.Button();
+            this.btneditar = new System.Windows.Forms.Button();
+            this.btnnuevo = new System.Windows.Forms.Button();
+            this.grdRecibos = new System.Windows.Forms.DataGridView();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdRecibos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.btnirUltimo);
+            this.panel1.Controls.Add(this.btnsiguiente);
+            this.panel1.Controls.Add(this.btnanterior);
+            this.panel1.Controls.Add(this.btnirPrimero);
+            this.panel1.Controls.Add(this.btnbuscar);
+            this.panel1.Controls.Add(this.btnimprimir);
+            this.panel1.Controls.Add(this.btnrefrescar);
+            this.panel1.Controls.Add(this.btncancelar);
+            this.panel1.Controls.Add(this.btnguardar);
+            this.panel1.Controls.Add(this.btneliminar);
+            this.panel1.Controls.Add(this.btneditar);
+            this.panel1.Controls.Add(this.btnnuevo);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(659, 55);
+            this.panel1.TabIndex = 5;
+            // 
+            // btnirUltimo
+            // 
+            this.btnirUltimo.Image = ((System.Drawing.Image)(resources.GetObject("btnirUltimo.Image")));
+            this.btnirUltimo.Location = new System.Drawing.Point(601, 4);
+            this.btnirUltimo.Name = "btnirUltimo";
+            this.btnirUltimo.Size = new System.Drawing.Size(48, 42);
+            this.btnirUltimo.TabIndex = 16;
+            this.btnirUltimo.UseVisualStyleBackColor = true;
+            this.btnirUltimo.Click += new System.EventHandler(this.btnirUltimo_Click);
+            // 
+            // btnsiguiente
+            // 
+            this.btnsiguiente.Image = ((System.Drawing.Image)(resources.GetObject("btnsiguiente.Image")));
+            this.btnsiguiente.Location = new System.Drawing.Point(547, 4);
+            this.btnsiguiente.Name = "btnsiguiente";
+            this.btnsiguiente.Size = new System.Drawing.Size(48, 42);
+            this.btnsiguiente.TabIndex = 15;
+            this.btnsiguiente.UseVisualStyleBackColor = true;
+            this.btnsiguiente.Click += new System.EventHandler(this.btnsiguiente_Click);
+            // 
+            // btnanterior
+            // 
+            this.btnanterior.Image = ((System.Drawing.Image)(resources.GetObject("btnanterior.Image")));
+            this.btnanterior.Location = new System.Drawing.Point(493, 4);
+            this.btnanterior.Name = "btnanterior";
+            this.btnanterior.Size = new System.Drawing.Size(48, 42);
+            this.btnanterior.TabIndex = 14;
+            this.btnanterior.UseVisualStyleBackColor = true;
+            this.btnanterior.Click += new System.EventHandler(this.btnanterior_Click);
+            // 
+            // btnirPrimero
+            // 
+            this.btnirPrimero.Image = ((System.Drawing.Image)(resources.GetObject("btnirPrimero.Image")));
+            this.btnirPrimero.Location = new System.Drawing.Point(439, 4);
+            this.btnirPrimero.Name = "btnirPrimero";
+            this.btnirPrimero.Size = new System.Drawing.Size(48, 42);
+            this.btnirPrimero.TabIndex = 13;
+            this.btnirPrimero.UseVisualStyleBackColor = true;
+            this.btnirPrimero.Click += new System.EventHandler(this.btnirPrimero_Click);
+            // 
+            // btnbuscar
+            // 
+            this.btnbuscar.Image = ((System.Drawing.Image)(resources.GetObject("btnbuscar.Image")));
+            this.btnbuscar.Location = new System.Drawing.Point(385, 4);
+            this.btnbuscar.Name = "btnbuscar";
+            this.btnbuscar.Size = new System.Drawing.Size(48, 42);
+            this.btnbuscar.TabIndex = 12;
+            this.btnbuscar.UseVisualStyleBackColor = true;
+            this.btnbuscar.Click += new System.EventHandler(this.btnbuscar_Click);
+            // 
+            // btnimprimir
+            // 
+            this.btnimprimir.Image = ((System.Drawing.Image)(resources.GetObject("btnimprimir.Image")));
+            this.btnimprimir.Location = new System.Drawing.Point(331, 4);
+            this.btnimprimir.Name = "btnimprimir";
+            this.btnimprimir.Size = new System.Drawing.Size(48, 42);
+            this.btnimprimir.TabIndex = 11;
+            this.btnimprimir.UseVisualStyleBackColor = true;
+            // 
+            // btnrefrescar
+            // 
+            this.btnrefrescar.Image = ((System.Drawing.Image)(resources.GetObject("btnrefrescar.Image")));
+            this.btnrefrescar.Location = new System.Drawing.Point(277, 4);
+            this.btnrefrescar.Name = "btnrefrescar";
+            this.btnrefrescar.Size = new System.Drawing.Size(48, 42);
+            this.btnrefrescar.TabIndex = 10;
+            this.btnrefrescar.UseVisualStyleBackColor = true;
+            // 
+            // btncancelar
+            // 
+            this.btncancelar.Image = ((System.Drawing.Image)(resources.GetObject("btncancelar.Image")));
+            this.btncancelar.Location = new System.Drawing.Point(223, 4);
+            this.btncancelar.Name = "btncancelar";
+            this.btncancelar.Size = new System.Drawing.Size(48, 42);
+            this.btncancelar.TabIndex = 9;
+            this.btncancelar.UseVisualStyleBackColor = true;
+            this.btncancelar.Click += new System.EventHandler(this.btncancelar_Click);
+            // 
+            // btnguardar
+            // 
+            this.btnguardar.Image = ((System.Drawing.Image)(resources.GetObject("btnguardar.Image")));
+            this.btnguardar.Location = new System.Drawing.Point(169, 4);
+            this.btnguardar.Name = "btnguardar";
+            this.btnguardar.Size = new System.Drawing.Size(48, 42);
+            this.btnguardar.TabIndex = 8;
+            this.btnguardar.UseVisualStyleBackColor = true;
+            // 
+            // btneliminar
+            // 
+            this.btneliminar.Image = ((System.Drawing.Image)(resources.GetObject("btneliminar.Image")));
+            this.btneliminar.Location = new System.Drawing.Point(115, 4);
+            this.btneliminar.Name = "btneliminar";
+            this.btneliminar.Size = new System.Drawing.Size(48, 42);
+            this.btneliminar.TabIndex = 7;
+            this.btneliminar.UseVisualStyleBackColor = true;
+            // 
+            // btneditar
+            // 
+            this.btneditar.Image = ((System.Drawing.Image)(resources.GetObject("btneditar.Image")));
+            this.btneditar.Location = new System.Drawing.Point(61, 4);
+            this.btneditar.Name = "btneditar";
+            this.btneditar.Size = new System.Drawing.Size(48, 42);
+            this.btneditar.TabIndex = 6;
+            this.btneditar.UseVisualStyleBackColor = true;
+            // 
+            // btnnuevo
+            // 
+            this.btnnuevo.Image = ((System.Drawing.Image)(resources.GetObject("btnnuevo.Image")));
+            this.btnnuevo.Location = new System.Drawing.Point(7, 4);
+            this.btnnuevo.Name = "btnnuevo";
+            this.btnnuevo.Size = new System.Drawing.Size(48, 42);
+            this.btnnuevo.TabIndex = 5;
+            this.btnnuevo.UseVisualStyleBackColor = true;
+            // 
+            // grdRecibos
+            // 
+            this.grdRecibos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdRecibos.Location = new System.Drawing.Point(87, 178);
+            this.grdRecibos.Name = "grdRecibos";
+            this.grdRecibos.Size = new System.Drawing.Size(535, 209);
+            this.grdRecibos.TabIndex = 6;
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Enabled = false;
+            this.txtBuscar.Location = new System.Drawing.Point(290, 126);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(189, 20);
+            this.txtBuscar.TabIndex = 7;
+            this.txtBuscar.KeyUp += new System.Windows.Forms.KeyEventHandler(this.txtBuscar_KeyUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(221, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "No. Recibo";
+            // 
+            // frmConsultaRecibos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(691, 415);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.grdRecibos);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.Name = "frmConsultaRecibos";
+            this.Text = "frmConsultaRecibos";
+            this.Load += new System.EventHandler(this.frmConsultaRecibos_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grdRecibos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnirUltimo;
+        private System.Windows.Forms.Button btnsiguiente;
+        private System.Windows.Forms.Button btnanterior;
+        private System.Windows.Forms.Button btnirPrimero;
+        private System.Windows.Forms.Button btnbuscar;
+        private System.Windows.Forms.Button btnimprimir;
+        private System.Windows.Forms.Button btnrefrescar;
+        private System.Windows.Forms.Button btncancelar;
+        private System.Windows.Forms.Button btnguardar;
+        private System.Windows.Forms.Button btneliminar;
+        private System.Windows.Forms.Button btneditar;
+        private System.Windows.Forms.Button btnnuevo;
+        private System.Windows.Forms.DataGridView grdRecibos;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ ProyectoConSeguridad/Login/frmConsultaRecibos.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Navegador;
+
+namespace Login
+{
+    public partial class frmConsultaRecibos : Form
+    {
+        public frmConsultaRecibos()
+        {
+            InitializeComponent();
+            funActualizar();
+        }
+
+        private void frmConsultaRecibos_Load(object sender, EventArgs e)
+        {
+            clasnegocio nuevo = new clasnegocio();
+            nuevo.funactivarDesactivarBoton(btnnuevo, Convert.ToBoolean(Permisos.btnnuevo));
+            nuevo.funactivarDesactivarBoton(btneditar, Convert.ToBoolean(Permisos.btneditar));
+            nuevo.funactivarDesactivarBoton(btneliminar, Convert.ToBoolean(Permisos.btneliminar));
+            nuevo.funactivarDesactivarBoton(btnguardar, Convert.ToBoolean(Permisos.btnguardar));
+            nuevo.funactivarDesactivarBoton(btncancelar, Convert.ToBoolean(Permisos.btncancelar));
+            nuevo.funactivarDesactivarBoton(btnrefrescar, Convert.ToBoolean(Permisos.btnrefrescar));
+            nuevo.funactivarDesactivarBoton(btnimprimir, Convert.ToBoolean(Permisos.btnimprimir));
+            nuevo.funactivarDesactivarBoton(btnbuscar, Convert.ToBoolean(Permisos.btnbuscar));
+            nuevo.funactivarDesactivarBoton(btnirPrimero, Convert.ToBoolean(Permisos.btnirPrimiero));
+            nuevo.funactivarDesactivarBoton(btnanterior, Convert.ToBoolean(Permisos.btnanterior));
+            nuevo.funactivarDesactivarBoton(btnsiguiente, Convert.ToBoolean(Permisos.btnsiguiente));
+            nuevo.funactivarDesactivarBoton(btnirUltimo, Convert.ToBoolean(Permisos.btnirUltimo));
+            btnnuevo.Enabled = false;
+            btneditar.Enabled = false;
+            btneliminar.Enabled = false;
+            btnguardar.Enabled = false;
+            btncancelar.Enabled = false;
+            btnrefrescar.Enabled = false;
+            btnimprimir.Enabled = false;
+
+        }
+        void funActualizar()
+        {
+            clasnegocio cnegocio = new clasnegocio();
+            cnegocio.funconsultarRegistros("trdocumentos", "SELECT trdocumentos.ncoddocumento as Recibo, trdocumentos.dfecha as Fecha , trdocumentos.nmonto as Monto from mafacturacobro,trdocumentos, matipotransaccion, trcobro WHERE matipotransaccion.vnombredocumento = 'Cobro'", "consulta", grdRecibos);
+        }
+
+        private void btnbuscar_Click(object sender, EventArgs e)
+        {
+            btncancelar.Enabled = true;
+            btnbuscar.Enabled = false;
+            txtBuscar.Enabled = true;
+        }
+
+        private void btnirPrimero_Click(object sender, EventArgs e)
+        {
+            clasnegocio cnegocio = new clasnegocio();
+            cnegocio.funPrimero(grdRecibos);
+        }
+
+        private void btnanterior_Click(object sender, EventArgs e)
+        {
+            clasnegocio cnegocio = new clasnegocio();
+            cnegocio.funSiguiente(grdRecibos);
+        }
+
+        private void btnsiguiente_Click(object sender, EventArgs e)
+        {
+            clasnegocio cnegocio = new clasnegocio();
+            cnegocio.funSiguiente(grdRecibos);
+        }
+
+        private void btnirUltimo_Click(object sender, EventArgs e)
+        {
+            clasnegocio cnegocio = new clasnegocio();
+            cnegocio.funUltimo(grdRecibos);
+        }
+
+        private void txtBuscar_KeyUp(object sender, KeyEventArgs e)
+        {
+            clasnegocio cnegocio = new clasnegocio();
+            cnegocio.funconsultarRegistros("trdocumentos", "SELECT trdocumentos.ncoddocumento as Codigo, trdocumentos.dfecha as Fecha , trdocumentos.nmonto as Monto from trdocumentos, matipotransaccion WHERE matipotransaccion.vnombredocumento = 'Cobro' AND trdocumentos.ncoddocumento LIKE '" + txtBuscar.Text + "%'", "consulta", grdRecibos);
+        }
+
+        private void btncancelar_Click(object sender, EventArgs e)
+        {
+            btncancelar.Enabled = false;
+            btnbuscar.Enabled = true;
+            txtBuscar.Enabled = false;
+        }
+    }
+}
